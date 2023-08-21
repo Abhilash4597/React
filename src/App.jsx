af2e267bb380304c2fa8a3607a2e88d336f277b7
Abhilash4597 <abhilash4597@gmail.com>
2023-08-21 16:15:02 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import { Component } from 'react';
+import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
 
-  return (
-    <>
-      
-    </>
-  )
+    this.state = {
+      monsters: [
+        {
+          id: 1,
+          name: 'Frankenstein',
+        },
+        {
+          id: 2,
+          name: 'Dracula',
+        },
+        {
+          id: 3,
+          name: 'Zombie',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
