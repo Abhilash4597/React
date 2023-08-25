1707dc1b5e21db33c4dc757c075bc2d6b242d7fc
Abhilash4597 <abhilash4597@gmail.com>
2023-08-25 17:37:05 +0530

    update

--- Components/Card-list/Card-list.component.jsx
+++ Components/Card-list/Card-list.component.jsx
@@ -1,6 +1,10 @@
-// import React from 'react';
+import PropTypes from 'prop-types';
 import './card-list.styles.css';
 
 export const CardList = props => {
   return <div className="card-list">{props.children}</div>;
+};
+
+CardList.propTypes = {
+  children: PropTypes.node.isRequired, // Use PropTypes.node for the children prop
 };
