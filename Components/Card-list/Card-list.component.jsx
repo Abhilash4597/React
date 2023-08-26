09c1798d5bab987fded90e97069d4a391eb56a01
Abhilash4597 <abhilash4597@gmail.com>
2023-08-26 13:15:57 +0530

    update

--- Components/Card-list/Card-list.component.jsx
+++ Components/Card-list/Card-list.component.jsx
@@ -2,9 +2,26 @@ import PropTypes from 'prop-types';
 import './card-list.styles.css';
 
 export const CardList = props => {
-  return <div className="card-list">{props.children}</div>;
+  //# Add a check for props.monsters before mapping
+  if (!props.monster) {
+    return <div>No monsters to display</div>;
+  }
+
+  return (
+    <div className="card-list">
+      {props.monster.map(monster => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
+    </div>
+  );
 };
 
+
 CardList.propTypes = {
-  children: PropTypes.node.isRequired, // Use PropTypes.node for the children prop
+  monster: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
 };
